a817f5b4740baa4a699233c3d2c6b327855f7246
kaushiksk <kaushikskalmady@gmail.com>
2017-03-28 00:07:17 +0530

    Reorganise and calls

--- Project-Code/ui/app.py
+++ Project-Code/ui/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask import jsonify
+from .utils import word_vectors
 
 app = Flask(__name__,static_url_path="/static") 
 
@@ -14,6 +15,13 @@ def reply():
 def index(): 
     return render_template("index.html")
 
+@app.rout('/words/relationships', methods=['POST'])
+def relationship():
+	start1, end1, start2 = json.loads(request.form['data'])
+	try:
+		return word_vectors.relationship(start1,end1,start2)
+	except:
+		return jsonify({'error': 'word not in index'})
 
 if (__name__ == "__main__"): 
     app.run(port = 5000) 
